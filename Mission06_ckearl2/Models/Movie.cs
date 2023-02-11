e6d70a6aaf2608e35e42e4be0512aa768ed425c1
Christopher Kearl <c2_kearl@yahoo.com>
2023-02-11 11:34:13 -0700

    Add project files.

---
+++ Mission06_ckearl2/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_ckearl2.Models
+{
+    public class Movie
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Rating { get; set; }
+        public bool Edited { get;  set; } 
+        public string LentTo { get; set; }
+        public string Notes { get; set; }
+    }
+}
